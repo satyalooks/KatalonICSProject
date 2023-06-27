4e4695974bf07d49c14df73fba1b4e1ca22c4347
jnalla <jnalla@AIPLDVM002.adaequare.com>
2023-06-27 11:44:59 +0530

    Test Suit

---
+++ Test Suites/ICSPortalLoginandLogout.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ICSPortalLoginandLogout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0708db3b-c8f8-4378-bd35-bb22c7d85257</testSuiteGuid>
+   <testCaseLink>
+      <guid>2ab8d36b-7e72-48a3-8bbd-e9a38afa77a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ICS Project/Inventory Module/LoginandLogoutScenario</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
